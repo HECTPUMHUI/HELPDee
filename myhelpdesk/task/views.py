ee478e530a726a0dcdde44288d8b451de0deccbb
andriimerkulov <forsoctask@gmail.com>
2023-03-19 20:12:23 +0200

    add some views, add Task model, create first templates

--- myhelpdesk/task/views.py
+++ myhelpdesk/task/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'task/index.html')
+
+
+def about(request):
+    return render(request, 'task/about.html')
+
+
+def degrees(request, degid):
+    return HttpResponse(f'<h1>Degrees</h1><p>{degid}</p>')
